23c4ff9aacbf8dfd6b529556ee964cd846ee63b8
@giancarlosgenao7 <giancarlosgenao7@gmail.com>
2024-04-01 11:27:51 -0400

    new pages visita

--- src/app/pages/layout/pages/visita/visita.component.ts
+++ src/app/pages/layout/pages/visita/visita.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-visita',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule , MatButtonModule , MatIconModule],
   templateUrl: './visita.component.html',
   styleUrl: './visita.component.css'
 })
